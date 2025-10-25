86eaa2ebd7283cfb008571859a7fdb5e9258f496
Shovi <jcusiq@unsa.edu.pe>
2025-10-25 18:51:50 -0500

    Ejercicio 6 Version iterativa

--- laboratorio11/Ej6.js
+++ laboratorio11/Ej6.js
@@ -1,7 +1,13 @@
-let numero = parseInt(prompt("Ingrese un numero:"));
-let mensaje = "La tabla de multiplicar del " + numero + " es:\n";
-for (let i = 1; i <= 12; i++) {
-    let resultado = numero * i;
-    mensaje += numero + " x " + i + " = " + resultado + "\n";
+let continuar = true;
+let numero;
+let mensaje;
+while (continuar)  {
+    let numero = parseInt(prompt("Ingrese un numero:"));
+    mensaje = "La tabla de multiplicar del " + numero + " es:\n";
+    for (let i = 1; i <= 12; i++) {
+        let resultado = numero * i;
+        mensaje += numero + " x " + i + " = " + resultado + "\n";
+    }
+    alert(mensaje);
+    continuar = confirm("¿Desea ver la tabla de otro número?");
 }
-alert(mensaje);

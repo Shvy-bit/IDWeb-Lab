ed5ddfddc344cc3421b453ee77b0ad8e8a184d60
Shvy-bit <jcusiq@unsa.edu.pe>
2025-10-30 19:58:33 -0600

    Ejercicio 6

--- laboratorio12/Ejercicios.js
+++ laboratorio12/Ejercicios.js
@@ -1,4 +1,4 @@
-function Ejercicio1() {
+function Ejercicio3() {
     let numero = parseInt(prompt("Ingrese un número para verificar si es par:"));
     if (esPar(numero)) {
         alert("El número " + numero + " es par.");
@@ -8,7 +8,7 @@ function Ejercicio1() {
 }function esPar(numero) {
     return numero % 2 === 0;
 }
-function Ejercicio2() {
+function Ejercicio4() {
     let base = parseFloat(prompt("Ingrese la base del rectángulo:"));
     let altura = parseFloat(prompt("Ingrese la altura del rectángulo:"));
     alert("El área del rectángulo es: " + areaRectangulo(base, altura).toFixed(2));
@@ -16,7 +16,7 @@ function Ejercicio2() {
 function areaRectangulo(base, altura) {
     return base * altura;
 }
-function Ejercicio3() {
+function Ejercicio5() {
     let numero1 = parseInt(prompt("Ingrese el primer número:"));
     let numero2 = parseInt(prompt("Ingrese el segundo número:"));
     if (esMultiplo(numero1, numero2)) {
@@ -27,4 +27,13 @@ function Ejercicio3() {
 }
 function esMultiplo(num1, num2) {
     return num1 % num2 === 0;
+}
+function Ejercicio6() {
+    let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+    let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+    let numero3 = parseFloat(prompt("Ingrese el tercer número:"));
+    alert("El mayor de los tres números es: " + mayorDeTres(numero1, numero2, numero3));
+}
+function mayorDeTres(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
 }
